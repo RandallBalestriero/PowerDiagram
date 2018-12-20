38a3f46a92de127921e51d41416fc38e817063d8
RandallBalestriero <randallbalestriero@gmail.com>
2018-12-20 04:53:56 -0600

    test

---
+++ run_pretraining.sh
@@ -0,0 +1,7 @@
+screen -dmS A bash -c "sleep 1;export CUDA_VISIBLE_DEVICES='0';python run_bias_constraint.py 0 0 constrained CIFAR";
+screen -dmS B bash -c "sleep 1;export CUDA_VISIBLE_DEVICES='1';python run_bias_constraint.py 0 0 unconstrained CIFAR";
+screen -dmS C bash -c "sleep 1;export CUDA_VISIBLE_DEVICES='3';python run_bias_constraint.py 0 0 zero CIFAR";
+screen -dmS D bash -c "sleep 1;export CUDA_VISIBLE_DEVICES='2';python run_bias_constraint.py 0 1 constrained CIFAR";
+screen -dmS E bash -c "sleep 1;export CUDA_VISIBLE_DEVICES='5';python run_bias_constraint.py 0 1 unconstrained CIFAR";
+screen -dmS F bash -c "sleep 1;export CUDA_VISIBLE_DEVICES='4';python run_bias_constraint.py 0 1 zero CIFAR";
+
